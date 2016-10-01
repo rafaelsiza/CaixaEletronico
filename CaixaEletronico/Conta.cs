eef7f98a336a7cba256489f6c0f3d693146a001b
@rafaelsiza <rafaelsiza@gmail.com>
2016-10-01 01:38:11 -0400

    Classe Conta inclusao dos metodos Sacar e Depositar

---
+++ CaixaEletronico/Conta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronico
+{
+    class Conta
+    {
+
+        public int numero;
+
+        public string titular;
+
+        public double saldo;
+
+
+        public void Saca(double valor) {
+
+            this.saldo -= valor;
+
+        }
+
+        public void Deposita(double valor)
+        {
+
+            this.saldo += valor;
+
+        }
+
+
+    }
+}
