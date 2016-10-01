d449507f7aacb0773792dc22f475c2dd8d64fb00
@rafaelsiza <rafaelsiza@gmail.com>
2016-10-01 01:37:33 -0400

    Form

--- CaixaEletronico/Form1.cs
+++ CaixaEletronico/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace CaixaEletronico
 {
     public partial class Form1 : Form
@@ -15,6 +16,41 @@ namespace CaixaEletronico
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Conta umaConta = new Conta();
+
+            umaConta.numero = 1;
+            umaConta.titular = "Rafael Siza";
+            umaConta.saldo = 10000;
+
+            Conta outraConta = new Conta();
+            outraConta.numero = 2;
+            outraConta.titular = "Marta Siza";
+            outraConta.saldo = 100000;
+
+            MessageBox.Show("O titular da conta é " + umaConta.titular);
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Conta guilherme = new Conta();
+            Conta mauricio = new Conta();
+
+            guilherme.saldo = 1000;
+
+            MessageBox.Show("Saldo do Guilherme antes do saque " + guilherme.saldo);
+
+            guilherme.Saca(100);
+
+            MessageBox.Show("Saldo do Guilherme depois do saque " + guilherme.saldo);
+
         }
     }
 }
